a2667e1aacb2489c7774b1e16f73288a32c34a1e
Caleb <c@leb.email>
2020-04-22 09:34:51 -0400

    Remove unnecessary trailing backslashes

--- refresh.sh
+++ refresh.sh
@@ -11,10 +11,10 @@ node /home/pi/spindrift/screenshot.js "$SPOT"
 
 # Send a command through Chrome's DevTools Protocol.
 devtools() {
-    /bin/echo "$@" |\
-    /usr/local/bin/websocat -n1 --jsonrpc $(\
-    /usr/bin/curl -sk http://127.0.0.1:9222/json |\
-    /usr/bin/jq '.[] | select(.url | index("report.html")) | .webSocketDebuggerUrl' -r \
+    /bin/echo "$@" |
+    /usr/local/bin/websocat -n1 --jsonrpc $(
+        /usr/bin/curl -sk http://127.0.0.1:9222/json |
+        /usr/bin/jq '.[] | select(.url | index("report.html")) | .webSocketDebuggerUrl' -r
     )
 }
 
