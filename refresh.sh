69fae048034aa0010bec4f047a6f0a4752edae28
Caleb <c@leb.email>
2020-04-22 09:39:22 -0400

    Remove unnecessary absolute paths to commands

--- refresh.sh
+++ refresh.sh
@@ -11,13 +11,13 @@ node /home/pi/spindrift/screenshot.js "$SPOT"
 
 # Send a command through Chrome's DevTools Protocol.
 devtools() {
-    /bin/echo "$@" |
-    /usr/local/bin/websocat -n1 --jsonrpc $(
-        /usr/bin/curl -sk http://127.0.0.1:9222/json |
-        /usr/bin/jq '.[] | select(.url | index("report.html")) | .webSocketDebuggerUrl' -r
+    echo "$@" |
+    websocat -n1 --jsonrpc $(
+        curl -sk http://127.0.0.1:9222/json |
+        jq '.[] | select(.url | index("report.html")) | .webSocketDebuggerUrl' -r
     )
 }
 
 # Reload page.
-/bin/echo 'Reloading page.'
+echo 'Reloading page.'
 devtools 'Page.reload'
